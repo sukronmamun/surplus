550885b0923c8e3d1eee46e1b87091fb2d21fe55
Sukron ma'mun <sukron.jamboo@gmail.com>
2023-03-13 13:34:42 +0700

    setup migration table

---
+++ database/migrations/2023_03_13_063023_create_images_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('image', function (Blueprint $table) {
+            $table->integer("id")->autoIncrement();
+            $table->string('name');
+            $table->string('file');
+            $table->boolean('enable');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('image');
+    }
+};
---
+++ database/migrations/2023_03_13_063205_product_image.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+     /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_image', function (Blueprint $table) {
+            $table->integer('product_id')->constrained('product');
+            $table->integer('image_id')->constrained('image');
+            $table->primary(['image_id', 'product_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_image');
+
+    }
+};
